74560a870527d8fc3346b4352b73162dc7f9620b
aviv9978 <85827588+aviv9978@users.noreply.github.com>
2023-01-23 20:59:54 +0200

    Starting project, navigation bar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDP3Y7e7AMHKDnMOzsF11yhKKAz_68RcoE',
+    authDomain: 'oshop-84b9e.firebaseapp.com',
+    projectId: 'oshop-84b9e',
+    storageBucket: 'oshop-84b9e.appspot.com',
+    messagingSenderId: '1047008151153',
+    appId: '1:1047008151153:web:2f98ec29b725c478453dd0',
+  },
 };
 
 /*
